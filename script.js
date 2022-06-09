577a6605619126aefe433900a799d9f194300571
David Henrique Araujo <95188952+Davidharaujo@users.noreply.github.com>
2022-06-09 10:18:00 -0300

    Aula 1

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = "David"
+let numero = 85
+let anoAtual = 2022
+
+let anoSubtrai = anoAtual - numero
+
+console.log(anoSubtrai)
+
+console.log(anoSubtrai + numero)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
